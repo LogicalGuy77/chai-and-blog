74091d72441dc5abf8f95740f9937522fa788af1
LogicalGuy77 <harshitacademia@gmail.com>
2024-06-29 17:16:24 +0530

    finalized app

---
+++ README.md
@@ -0,0 +1,10 @@
+# React + Appwrite
+
+This project was made following the 'Chai Aur React' playlist on tutorial.
+Following the best code practices and conventions.
+
+# Challenges faced
+
+At first the app wasn't working at all and took total for 4 days to debug. Final problem I faced was that after login `userData` was stored in redux state but wasn't showing until I would refresh the page once after login. I tried multiple approaches to fix it but at last was bound to hotfix it by adding `window.reload` after login.
+
+Any suggestion/issue is appreciated and can be raised in the GitHub issues section.
---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Logo from "./Logo";
+import Container from "./Container/Container";
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import Input from "./Input";
+import Button from "./Button";
+import Select from "./Select";
+import Signup from "./Signup";
+import Login from "./Login";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+
+export {
+  Header,
+  Footer,
+  Logo,
+  Container,
+  LogoutBtn,
+  RTE,
+  Input,
+  Button,
+  Select,
+  Signup,
+  Login,
+  PostForm,
+  PostCard,
+  AuthLayout,
+};
